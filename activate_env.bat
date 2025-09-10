c7de30b1756fb3f060c255680e3a3103399c9882
brylle <b_xavace@outlook.com>
2025-09-10 19:47:37 +0800

    Remove environment activation scripts for moodeng_env

--- activate_env.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Activating moodeng_env virtual environment...
-call moodeng_env\Scripts\activate.bat
-echo.
-echo Virtual environment activated!
-echo You can now run: python model_training_enhanced.py
-echo.
-cmd /k
